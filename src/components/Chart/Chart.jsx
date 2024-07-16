d182e9f474ea3afe22886474bb4f443209629678
Amsndakdmd <amsndakdmd@gmail.com>
2024-07-16 13:51:34 +0200

    Update chart

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -18,6 +18,145 @@ ChartJS.register(
   Filler
 )
 
-export default function Chart({ options, data }) {
+export default function Chart({
+  data: chartData,
+  labels: chartLabels,
+  callbacks: chartCallbacks,
+}) {
+  const data = {
+    labels: chartLabels,
+    datasets: [
+      {
+        data: chartData,
+        backgroundColor: (context) => {
+          if (!context.chart.chartArea) return
+          const {
+            ctx,
+            chartArea: { top, bottom },
+          } = context.chart
+
+          const gradientBackground = ctx.createLinearGradient(0, top, 0, bottom)
+          gradientBackground.addColorStop(
+            0,
+            getComputedStyle(document.documentElement).getPropertyValue(
+              '--chart-gradient-start-color'
+            )
+          )
+          gradientBackground.addColorStop(
+            1,
+            getComputedStyle(document.documentElement).getPropertyValue(
+              '--chart-gradient-end-color'
+            )
+          )
+          return gradientBackground
+        },
+        fill: 'start',
+        tension: 0.25,
+        borderColor: getComputedStyle(
+          document.documentElement
+        ).getPropertyValue('--brand-500'),
+        borderWidth: 4,
+        pointBackgroundColor: 'rgba(0,0,0,0)',
+        pointBorderColor: 'rgba(0,0,0,0)',
+        pointHoverBackgroundColor: getComputedStyle(
+          document.documentElement
+        ).getPropertyValue('--brand-500'),
+        pointHoverBorderColor: 'white',
+        pointHoverBorderWidth: 2,
+      },
+    ],
+  }
+
+  const options = {
+    responsive: true,
+    interaction: {
+      mode: 'index',
+      intersect: false,
+    },
+    scales: {
+      y: {
+        min: 0,
+        grid: {
+          color: getComputedStyle(document.documentElement).getPropertyValue(
+            '--neutral-800'
+          ),
+        },
+        border: {
+          dash: [4, 3],
+        },
+        ticks: {
+          color: getComputedStyle(document.documentElement).getPropertyValue(
+            '--neutral-400'
+          ),
+          font: {
+            family: getComputedStyle(document.documentElement).getPropertyValue(
+              '--font-primary'
+            ),
+            size: getComputedStyle(document.documentElement).getPropertyValue(
+              '--paragraph-sm'
+            ),
+          },
+          callback: function (value) {
+            return value.toLocaleString('en-US', {
+              style: 'currency',
+              currency: 'USD',
+              notation: 'compact',
+            })
+          },
+        },
+      },
+      x: {
+        grid: {
+          display: false,
+        },
+        ticks: {
+          color: getComputedStyle(document.documentElement).getPropertyValue(
+            '--neutral-400'
+          ),
+          font: {
+            family: getComputedStyle(document.documentElement).getPropertyValue(
+              '--font-primary'
+            ),
+            size: getComputedStyle(document.documentElement).getPropertyValue(
+              '--paragraph-sm'
+            ),
+          },
+        },
+      },
+    },
+    plugins: {
+      tooltip: {
+        yAlign: 'bottom',
+        displayColors: false,
+        cornerRadius: 4,
+        titleFont: {
+          family: getComputedStyle(document.documentElement).getPropertyValue(
+            '--font-primary'
+          ),
+          size: getComputedStyle(document.documentElement).getPropertyValue(
+            '--heading4'
+          ),
+          weight: 500,
+        },
+        bodyFont: {
+          family: getComputedStyle(document.documentElement).getPropertyValue(
+            '--font-primary'
+          ),
+          size: getComputedStyle(document.documentElement).getPropertyValue(
+            '--paragraph-sm'
+          ),
+        },
+        callbacks: {
+          ...chartCallbacks,
+          labelTextColor: () => {
+            return getComputedStyle(document.documentElement).getPropertyValue(
+              '--neutral-400'
+            )
+          },
+        },
+      },
+    },
+  }
+
   return <Line options={options} data={data} />
 }
--- src/sections/BalanceSection/BalanceSection.jsx
+++ src/sections/BalanceSection/BalanceSection.jsx
@@ -13,149 +13,6 @@ export default function BalanceSection() {
   const { getLastBalance, balance } = useBalance()
   const dialogRef = useRef(null)
 
-  const data = {
-    labels: balance.map(({ date }) => date),
-    datasets: [
-      {
-        data: balance.map(({ balance }) => balance),
-        backgroundColor: (context) => {
-          if (!context.chart.chartArea) return
-          const {
-            ctx,
-            chartArea: { top, bottom },
-          } = context.chart
-
-          const gradientBackground = ctx.createLinearGradient(0, top, 0, bottom)
-          gradientBackground.addColorStop(
-            0,
-            getComputedStyle(document.documentElement).getPropertyValue(
-              '--chart-gradient-start-color'
-            )
-          )
-          gradientBackground.addColorStop(
-            1,
-            getComputedStyle(document.documentElement).getPropertyValue(
-              '--chart-gradient-end-color'
-            )
-          )
-          return gradientBackground
-        },
-        fill: 'start',
-        tension: 0.25,
-        borderColor: getComputedStyle(
-          document.documentElement
-        ).getPropertyValue('--brand-500'),
-        borderWidth: 4,
-        pointBackgroundColor: 'rgba(0,0,0,0)',
-        pointBorderColor: 'rgba(0,0,0,0)',
-        pointHoverBackgroundColor: getComputedStyle(
-          document.documentElement
-        ).getPropertyValue('--brand-500'),
-        pointHoverBorderColor: 'white',
-        pointHoverBorderWidth: 2,
-      },
-    ],
-  }
-
-  const options = {
-    responsive: true,
-    interaction: {
-      mode: 'index',
-      intersect: false,
-    },
-    scales: {
-      y: {
-        min: 0,
-        grid: {
-          color: getComputedStyle(document.documentElement).getPropertyValue(
-            '--neutral-800'
-          ),
-        },
-        border: {
-          dash: [4, 3],
-        },
-
-        ticks: {
-          color: getComputedStyle(document.documentElement).getPropertyValue(
-            '--neutral-400'
-          ),
-          font: {
-            family: getComputedStyle(document.documentElement).getPropertyValue(
-              '--font-primary'
-            ),
-            size: getComputedStyle(document.documentElement).getPropertyValue(
-              '--paragraph-sm'
-            ),
-          },
-          callback: function (value) {
-            return value.toLocaleString('en-US', {
-              style: 'currency',
-              currency: 'USD',
-              notation: 'compact',
-            })
-          },
-        },
-      },
-      x: {
-        grid: {
-          display: false,
-        },
-        ticks: {
-          color: getComputedStyle(document.documentElement).getPropertyValue(
-            '--neutral-400'
-          ),
-          font: {
-            family: getComputedStyle(document.documentElement).getPropertyValue(
-              '--font-primary'
-            ),
-            size: getComputedStyle(document.documentElement).getPropertyValue(
-              '--paragraph-sm'
-            ),
-          },
-        },
-      },
-    },
-    plugins: {
-      tooltip: {
-        yAlign: 'bottom',
-        displayColors: false,
-        cornerRadius: 4,
-        titleFont: {
-          family: getComputedStyle(document.documentElement).getPropertyValue(
-            '--font-primary'
-          ),
-          size: getComputedStyle(document.documentElement).getPropertyValue(
-            '--heading4'
-          ),
-          weight: 500,
-        },
-        bodyFont: {
-          family: getComputedStyle(document.documentElement).getPropertyValue(
-            '--font-primary'
-          ),
-          size: getComputedStyle(document.documentElement).getPropertyValue(
-            '--paragraph-sm'
-          ),
-        },
-        callbacks: {
-          title: (ctx) => {
-            return currencyFormatter(
-              balance.map(({ balance }) => balance)[ctx[0].dataIndex]
-            )
-          },
-          label: (ctx) => {
-            return balance.map(({ date }) => date)[ctx.dataIndex]
-          },
-          labelTextColor: (ctx) => {
-            return getComputedStyle(document.documentElement).getPropertyValue(
-              '--neutral-400'
-            )
-          },
-        },
-      },
-    },
-  }
-
   return (
     <>
       <Section>
@@ -173,8 +30,20 @@ export default function BalanceSection() {
             Send Money
           </Button>
         </header>
-
-        <Chart options={options} data={data} />
+        <Chart
+          callbacks={{
+            title: (ctx) => {
+              return currencyFormatter(
+                balance.map(({ balance }) => balance)[ctx[0].dataIndex]
+              )
+            },
+            label: (ctx) => {
+              return balance.map(({ date }) => date)[ctx.dataIndex]
+            },
+          }}
+          labels={balance.map(({ date }) => date)}
+          data={balance.map(({ balance }) => balance)}
+        />
       </Section>
       <Dialog ref={dialogRef}>
         <SendMoneyModal closeModal={() => dialogRef?.current?.close()} />
